4f309de03aad7f58a920044b90b33e84a99e1399
Cho-hs0004 <huntmd0004@gmail.com>
2024-11-27 19:36:58 +0900

    Timer, Date, SkillUI, HintImage

---
+++ Phase_Runner/Assets/Scripts/Timer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public TMP_Text timerText;
+    public TMP_Text Date;
+    private int cur_day = 0;
+    private float timeRemaining = 180f; 
+    private bool timerRunning = true;
+
+    void Update()
+    {
+        if (timerRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                UpdateTimerDisplay(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerRunning = false;
+                TimerEnded();
+            }
+        }
+    }
+
+    void UpdateTimerDisplay(float timeToDisplay)
+    {
+       
+        int minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        int seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void TimerEnded()
+    {
+        cur_day++;
+        Date.text = "Day " + cur_day;
+    }
+}
