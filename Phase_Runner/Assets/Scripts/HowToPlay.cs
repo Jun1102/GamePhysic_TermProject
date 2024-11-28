3867994de0abd46dbce4ca5dac87272ae935819f
Cho-hs0004 <huntmd0004@gmail.com>
2024-11-29 00:28:57 +0900

    MainLobbyUpdate

---
+++ Phase_Runner/Assets/Scripts/HowToPlay.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HowToPlay : MonoBehaviour
+{
+    static public bool isHTPON = false;
+    public Button startBtn;
+    void Start()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isHTPON == true)
+        {
+            startBtn.GetComponent<Button>().enabled = false;
+            this.GetComponent<Button>().enabled = false;
+            startBtn.GetComponent<EventTrigger>().enabled = false;
+            this.GetComponent<EventTrigger>().enabled = false;
+        }
+        else if(isHTPON == false)
+        {
+            startBtn.GetComponent<Button>().enabled = true;
+            this.GetComponent<Button>().enabled = true;
+            startBtn.GetComponent<EventTrigger>().enabled = true;
+            this.GetComponent<EventTrigger>().enabled = true;
+        }
+    }
+
+
+    public void HTPON()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(true);
+        isHTPON = true;
+    }
+
+    public void HTPOFF()
+    {
+        this.transform.GetChild(0).gameObject.SetActive(false);
+        isHTPON=false;
+    }
+}
---
+++ Phase_Runner/Assets/Scripts/Off_Game.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Off_Game : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
