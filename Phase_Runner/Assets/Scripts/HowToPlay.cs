0c8af2c192ec514bb1108d4fbaba3233593dc74c
Cho-hs0004 <huntmd0004@gmail.com>
2024-11-29 13:36:44 +0900

    HowToPlayBtn

--- Phase_Runner/Assets/Scripts/HowToPlay.cs
+++ Phase_Runner/Assets/Scripts/HowToPlay.cs
@@ -8,6 +8,7 @@ public class HowToPlay : MonoBehaviour
 {
     static public bool isHTPON = false;
     public Button startBtn;
+    public Button quitBtn;
     void Start()
     {
         this.transform.GetChild(0).gameObject.SetActive(false);
@@ -18,15 +19,19 @@ public class HowToPlay : MonoBehaviour
         if (isHTPON == true)
         {
             startBtn.GetComponent<Button>().enabled = false;
+            quitBtn.GetComponent<Button>().enabled = false;
             this.GetComponent<Button>().enabled = false;
             startBtn.GetComponent<EventTrigger>().enabled = false;
+            quitBtn.GetComponent<EventTrigger>().enabled = false;
             this.GetComponent<EventTrigger>().enabled = false;
         }
         else if(isHTPON == false)
         {
             startBtn.GetComponent<Button>().enabled = true;
+            quitBtn.GetComponent<Button>().enabled = true;
             this.GetComponent<Button>().enabled = true;
             startBtn.GetComponent<EventTrigger>().enabled = true;
+            quitBtn.GetComponent<EventTrigger>().enabled = true;
             this.GetComponent<EventTrigger>().enabled = true;
         }
     }
